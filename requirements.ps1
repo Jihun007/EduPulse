e33b171dcf5fa529202b18880518cfdf059511ac
JieunHong <wpluscod@gmail.com>
2025-08-18 23:43:44 +0900

    requirements 설치 및 db, csv 초기화

--- requirements.ps1
+++ requirements.ps1
@@ -20,28 +20,27 @@ if ($versionMatch) {
         Write-Host "https://www.python.org/downloads/ 에서 최신 버전을 설치하세요."
         exit 1
     }
+
+    Write-Host "Python 버전 확인 성공! 현재 버전: $pythonVersion" -ForegroundColor Green
+    Write-Host ""
 } else {
     Write-Host "Python 버전 확인에 실패했습니다." -ForegroundColor Yellow
+    Write-Host ""
+    exit 1
 }
 
-Write-Host "Python 버전 확인 성공! 현재 버전: $pythonVersion" -ForegroundColor Green
+Write-Host "가상환경 설치 및 필수 패키지 설치중..."
 
-# 2) 가상환경 생성 (.venv 폴더)
-python -m venv .venv
-
-# 3) 가상환경 활성화
-..venv\Scripts\Activate.ps1
-
-# 4) pip 업그레이드
+# 2) pip 업그레이드
+Write-Host "pip 업그레이드 중..."
 python -m pip install --upgrade pip
 
-# 5) 필수 패키지 설치
-pip install streamlit==1.47.0
-pip install jupyter==7.3.2
-pip install sqlite-bro==0.14.1
-pip install matplotlib==3.10.0
-pip install openpyxl==3.1.0
-pip install numpy==2.2.6
+# 3) 가상환경 생성 (.venv 폴더)
+python -m venv .venv
+
+# 4) 가상환경 활성화 및 필수 패키지 설치
+.venv\Scripts\Activate.ps1
+pip install -r requirements.txt
 
 Write-Host ""
 Write-Host "가상환경 설정 및 패키지 설치가 완료되었습니다." -ForegroundColor Green
